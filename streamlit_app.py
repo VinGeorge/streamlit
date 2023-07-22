f8aa6f325113a035f9aaffe1a7e5239988ac6e29
Georgiy Vinogradov <Gorgevin@gmail.com>
2023-07-22 12:39:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Ouatmeal')
+streamlit.text('Kamle, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free Range egg')

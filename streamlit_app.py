0953f20d5cdabc871811d40cf0508a5dd8e1dd30
Georgiy Vinogradov <Gorgevin@gmail.com>
2023-07-23 12:07:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ except ERLError as e:
 streamlit.header("View Our Fruits List - Add Your Favorites:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-    return my_cur.fetchall()
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
 #Add the button to load the fruit
 if streamlit.button('Get Fruit Load List'):

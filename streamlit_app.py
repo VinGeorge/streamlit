741cd6705d18c7bc4ac51572083760b147fa915f
Georgiy Vinogradov <Gorgevin@gmail.com>
2023-07-23 11:54:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
 except ERLError as e:
   streamlit.error()
 
-streamlit.header("The fruits load list contains:")
+streamlit.header("View Our Fruits List - Add Your Favorites:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -48,13 +48,14 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 def inser_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
         return "Thanks for additing" + new_fruit
-
+        
 add_my_fruit = streamlit.text_input('What fruit would you like ito add?')
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
